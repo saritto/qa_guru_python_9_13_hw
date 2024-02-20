8976831f6c837a3e84d5e028d83ffcfc67c58c60
Sara Nurtazayeva <nurtazaeva.sara@gmail.com>
2024-02-21 00:32:45 +0600

    - added options

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 from selene import browser
 from utils import attach
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope='function', autouse=True)
@@ -8,10 +10,27 @@ def browser_management():
     browser.config.window_height = 720
     browser.config.window_width = 1280
 
-    yield
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
 
-    attach.add_html(browser)
+    browser.config.driver = driver
+    yield browser
+
     attach.add_screenshot(browser)
     attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
 
     browser.quit()
