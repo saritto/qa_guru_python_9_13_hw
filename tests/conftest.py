cc710aa77d0fa80f01725afad04b3403fac593ca
Sara Nurtazayeva <nurtazaeva.sara@gmail.com>
2024-02-20 16:22:00 +0600

    - added allure config - added attach.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from selene import browser
+from utils import attach
 
 
 @pytest.fixture(scope='function', autouse=True)
@@ -8,5 +9,9 @@ def browser_management():
     browser.config.window_width = 1280
 
     yield
+
+    attach.add_html(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
 
     browser.quit()
--- tests/test_demoqa_form.py
+++ tests/test_demoqa_form.py
@@ -1,12 +1,27 @@
 from demoqa_tests.data import users
 from demoqa_tests.pages.registration_page import RegistrationPage
+import allure
+from allure_commons.types import Severity
 
 
+@allure.tag("web")
+@allure.description("Проверка страницы Demoqa practice form")
+@allure.severity(Severity.NORMAL)
+@allure.label("owner", "saritto")
+@allure.link("https://demoqa.com/automation-practice-form", name="Demoqa form")
+@allure.title("Проверка страницы Demoqa practice form")
 def test_demoqa_form():
     registration_page = RegistrationPage()
     admin = users.admin
 
-    registration_page.open()
-    registration_page.register(admin)
-    registration_page.submit()
-    registration_page.should_have_registered(admin)
+    with allure.step("Открываем главную страницу:"):
+        registration_page.open()
+
+    with allure.step("Регистрируем пользователя:"):
+        registration_page.register(admin)
+
+    with allure.step("Отправляем заполненную форму:"):
+        registration_page.submit()
+
+    with allure.step("Открывается форма с зарегистрированными данными:"):
+        registration_page.should_have_registered(admin)
